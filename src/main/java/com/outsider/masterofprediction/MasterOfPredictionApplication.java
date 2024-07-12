1f4ad80f02c7a0f2b3d37a677ffddd32a6ebc18e
윤여원 <dudnjsckrgo@gmail.com>
2024-07-12 19:26:13 +0900

    project 생성

---
+++ src/main/java/com/outsider/masterofprediction/MasterOfPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.outsider.masterofprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOfPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOfPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outsider/masterofprediction/MasterOfPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsider.masterofprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterOfPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
