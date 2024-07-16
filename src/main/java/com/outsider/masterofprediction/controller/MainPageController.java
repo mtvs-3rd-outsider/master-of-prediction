5cf57819579065b10598cc2a4bb03a9d918daf41
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-16 20:41:56 +0900

    refactor change folder structure (#22)
    * feat: Controller 폴더 구조 변경
    
    * feat: 빈파일제거

--- src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
@@ -1,4 +1,4 @@
-package com.outsider.masterofprediction.controller.mainpage;
+package com.outsider.masterofprediction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/outsider/masterofprediction/controller/MyPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/MyPageController.java
@@ -1,4 +1,4 @@
-package com.outsider.masterofprediction.controller.mypage;
+package com.outsider.masterofprediction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
