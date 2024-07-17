051b766f93f8df569e519926ec15d363fb43b89b
apfp77 <apfp77@naver.com>
2024-07-17 19:31:53 +0900

    feat: 배팅 다중삭제 구현

--- src/main/java/com/outsider/masterofprediction/service/BettingDeleteService.java
+++ src/main/java/com/outsider/masterofprediction/service/BettingDeleteService.java
@@ -1,6 +1,7 @@
 package com.outsider.masterofprediction.service;
 
 import com.outsider.masterofprediction.mapper.AttachmentMapper;
+import com.outsider.masterofprediction.mapper.SubjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
