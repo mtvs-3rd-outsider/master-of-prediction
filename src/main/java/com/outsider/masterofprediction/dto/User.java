b0ee5ef018778009248eabb380ed95a6fde279a6
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-17 09:16:38 +0900

    23 feat user mappe (#24)
    * feat: 유저 Mapper, DTO 추가, MapperScan 추가
    
    * feat: 유저 Mapper, DTO 추가, MapperScan 추가
    
    * feat: 회원 Query DTO추가

---
+++ src/main/java/com/outsider/masterofprediction/dto/User.java
@@ -0,0 +1,50 @@
+package com.outsider.masterofprediction.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@ToString
+@Builder
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime joinDate;
+
+    private String authority;
+
+    private BigDecimal point;
+
+    private boolean withdrawalStatus;
+    private String provider; //어떤 OAuth인지(google, naver 등)
+    private String provideId; // 해당 OAuth 의 key(id)
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, LocalDateTime joinDate, String authority, BigDecimal point, boolean withdrawalStatus, String provider, String provideId) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.joinDate = joinDate;
+        this.authority = authority;
+        this.point = point;
+        this.withdrawalStatus = withdrawalStatus;
+        this.provider = provider;
+        this.provideId = provideId;
+    }
+}
