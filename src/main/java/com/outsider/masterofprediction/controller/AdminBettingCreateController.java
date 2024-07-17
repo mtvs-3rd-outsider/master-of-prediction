a4eecb595d3c7aee9f5b6a650e48d9cb3f25dd54
apfp77 <apfp77@naver.com>
2024-07-17 14:47:11 +0900

    feat: 배팅등록 서비스, Controller 추가

---
+++ src/main/java/com/outsider/masterofprediction/controller/AdminBettingCreateController.java
@@ -0,0 +1,43 @@
+package com.outsider.masterofprediction.controller;
+
+import com.outsider.masterofprediction.dto.TblSubjectDTO;
+import com.outsider.masterofprediction.dto.response.BettingAndAttachmentDTO;
+import com.outsider.masterofprediction.service.BettingCreateService;
+import com.outsider.masterofprediction.service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Controller
+@RequestMapping("/admin-page/betting/create")
+public class AdminBettingCreateController {
+
+    private final BettingCreateService bettingCreateService;
+    private final CategoryService categoryService;
+
+    public AdminBettingCreateController(BettingCreateService bettingCreateService, CategoryService categoryService) {
+        this.bettingCreateService = bettingCreateService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public String createNotification(Model model){
+
+        model.addAttribute("betting", new BettingAndAttachmentDTO());
+        model.addAttribute("categories", categoryService.findAll());
+        return "content/admin-page/betting/create";
+    }
+
+    @PostMapping
+    public String createBetting(@ModelAttribute TblSubjectDTO tblSubjectDTO,
+                                @RequestParam("attachmentFileAddress") MultipartFile     file,
+                                @RequestParam("deadline") Date date){
+        tblSubjectDTO.setSubjectSettlementTimestamp(new Timestamp(date.getTime()));
+        bettingCreateService.create(tblSubjectDTO, file);
+        return "redirect:/admin-page/betting";
+    }
+}
