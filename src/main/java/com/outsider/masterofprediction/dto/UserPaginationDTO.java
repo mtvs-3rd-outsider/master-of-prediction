0b580d3cf97044e8e513457bc1646cee02a1ab71
윤여원 <dudnjsckrgo@gmail.com>
2024-07-18 16:29:19 +0900

    feat: 페이지네이션 DTO , 상수 추가

--- src/main/java/com/outsider/masterofprediction/dto/UserPaginationDTO.java
+++ src/main/java/com/outsider/masterofprediction/dto/UserPaginationDTO.java
@@ -1,5 +1,12 @@
 package com.outsider.masterofprediction.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class UserPaginationDTO {
     private Long userId;
     private int start;
