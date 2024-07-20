4dd22ffc8c52d399ed18e2a2cd12559ec2e9873a
jeonjinhyun <jjh06500@gmail.com>
2024-07-20 16:16:48 +0900

    feat: 배팅페이지 댓글 밑 답글 구현 (#108)

---
+++ src/main/java/com/outsider/masterofprediction/mapper/ReplyMapper.java
@@ -0,0 +1,7 @@
+package com.outsider.masterofprediction.mapper;
+
+import com.outsider.masterofprediction.dto.TblReplyDTO;
+
+public interface ReplyMapper {
+    void insertReply(TblReplyDTO replyDTO);
+}
