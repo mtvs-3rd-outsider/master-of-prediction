ae7ad0082e7b1113d79421cee5ef6f8ec44e7ef1
yeowon <dudnjsckrgo@gmail.com>
2024-07-20 14:06:27 +0900

    feat: autologin init 변경 문의 테스트

--- src/main/java/com/outsider/masterofprediction/config/SecurityConfigAutoLogin.java
+++ src/main/java/com/outsider/masterofprediction/config/SecurityConfigAutoLogin.java
@@ -2,8 +2,12 @@ package com.outsider.masterofprediction.config;
 
 
 import com.outsider.masterofprediction.dto.TblAttachmentDTO;
+import com.outsider.masterofprediction.dto.TblCommentDTO;
+import com.outsider.masterofprediction.dto.TblInquiryDTO;
 import com.outsider.masterofprediction.dto.constatnt.StringConstants;
 import com.outsider.masterofprediction.mapper.AttachmentMapper;
+import com.outsider.masterofprediction.mapper.CommentMapper;
+import com.outsider.masterofprediction.mapper.InquiryMapper;
 import com.outsider.masterofprediction.mapper.UserMapper;
 import com.outsider.masterofprediction.service.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 import static com.outsider.masterofprediction.dto.constatnt.StringConstants.DEFAULT_USER_EMAIL;
 
 @Profile("autoLogin")
@@ -29,18 +36,22 @@ public class SecurityConfigAutoLogin {
     private final UserMapper userMapper;
 
     private final AttachmentMapper attachmentMapper;
+    private final InquiryMapper inquiryMapper;
 
     private final String email= DEFAULT_USER_EMAIL;
     private final String password="1";
     private final String role="ROLE_USER";
     private final String name="test";
     private final CustomUserService customUserService;
+    private final CommentMapper commentMapper;
 
     @Autowired
-    public SecurityConfigAutoLogin(UserMapper userMapper, AttachmentMapper attachmentMapper, CustomUserService customUserService) {
+    public SecurityConfigAutoLogin(UserMapper userMapper, AttachmentMapper attachmentMapper, InquiryMapper inquiryMapper, CustomUserService customUserService, CommentMapper commentMapper) {
         this.userMapper = userMapper;
         this.attachmentMapper = attachmentMapper;
+        this.inquiryMapper = inquiryMapper;
         this.customUserService = customUserService;
+        this.commentMapper = commentMapper;
     }
 
     @Bean
@@ -74,6 +85,19 @@ public class SecurityConfigAutoLogin {
                 tblAttachmentDTO.setAttachmentRegistUserNo(id);
                 tblAttachmentDTO.setAttachmentFileAddress("https://lh3.googleusercontent.com/a/ACg8ocKCH9UgnTgbHfYEi5WGYXXlaMd-uz3gSyTyXUz-w78BUCC38XA=s96-c");
                 attachmentMapper.setAttachmentsByAttachmentUserNo(tblAttachmentDTO);
+                TblInquiryDTO tblInquiryDTO = new TblInquiryDTO();
+                tblInquiryDTO.setInquiryUserNo(id);
+                // 현재 시간을 기반으로 Timestamp 객체 생성
+                tblInquiryDTO.setInquiryTitle("qewjkjkjkj");
+                tblInquiryDTO.setInquiryContent("qewdfsfsfsfsfjkjkjkj");
+                Timestamp inquiryTimestamp = Timestamp.from(Instant.now());
+                tblInquiryDTO.setInquiryTimestamp(inquiryTimestamp);
+                inquiryMapper.insertInquiry(tblInquiryDTO);
+//                TblCommentDTO tblCommentDTO = new TblCommentDTO();
+//                tblCommentDTO.setCommentUserNo(id);
+//                tblCommentDTO.setCommentContent("dfsfsf");
+//                tblCommentDTO.setCommentSubjectNo(1);
+//                commentMapper.
             }
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
                 userMapper.deleteUser(email);
