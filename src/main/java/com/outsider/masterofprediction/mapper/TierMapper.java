54e6ec37e06f6fe69cbec9852a2d24710ccb48b2
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-22 20:48:54 +0900

    관리자페이지에서 티어 생성 및 수정 (#139)
    * feat: 메인페이지 데이터베이스 연동
    
    * feat: 누락된 DTO 추가
    
    * feat: logo 여백 제거
    
    * feat: 관리자 사이드바에서 항목 클릭 시 색상 변경
    
    * feat: 파일 업로드 폴더 수정
    
    * feat: 헤더의 카테고리 수정
    
    * feat: 헤더 텍스트 수정
    
    * feat: 메인페이지에서 상품 선택시 상세페이지로 이동 url 수정
    
    * feat: 각 페이지 연결
    
    * feat: 티어 생성 ui 및 수정 ui

--- src/main/java/com/outsider/masterofprediction/mapper/TierMapper.java
+++ src/main/java/com/outsider/masterofprediction/mapper/TierMapper.java
@@ -9,13 +9,14 @@ import java.util.List;
 @Mapper
 public interface TierMapper {
 
-    @Select("SELECT * FROM tbl_user")
-    List<TblTierDTO> getAll();
+    @Select("SELECT * FROM tbl_tier")
+    List<TblTierDTO> findAll();
 
-    @Select("SELECT * FROM tbl_user LIMIT #{first}, #{count}")
-    List<TblTierDTO> getLimit(int first, int count);
+    @Select("SELECT * FROM tbl_tier LIMIT #{first}, #{count}")
+    List<TblTierDTO> findLimit(int first, int count);
 
     @Insert("INSERT INTO tbl_tier (tier_name, tier_content) VALUES (#{tierName}, #{tierContent})")
+    @Options(useGeneratedKeys = true, keyProperty = "tierNo")
     void insert(TblTierDTO tier);
 
     @Update("UPDATE tbl_tier SET tier_name = #{tierName}, tier_content = #{tierContent} WHERE tier_no = #{tierNo}")
@@ -27,4 +28,6 @@ public interface TierMapper {
     @DeleteProvider(type = DeleteByIdsProvider.class, method = "deleteTierByIdsProvider")
     void deleteByIds(List<Long> ids);
 
+    @Select("SELECT * FROM tbl_tier WHERE tier_no = #{tierNo}")
+    TblTierDTO findByTierNo(Long tierNo);
 }
