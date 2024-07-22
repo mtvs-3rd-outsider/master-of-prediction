38a71a71cdc49e416f64961c6801e449d5321b13
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-23 08:28:28 +0900

    티어 자동 갱신 추가 (#144)
    * feat: 티어 이미지 및 UserDTO에 티어 추가, 티어 DTO 추가
    
    * feat: 티어 5분간격으로 갱신기능 추가

---
+++ src/main/java/com/outsider/masterofprediction/service/TierScheduledTasks.java
@@ -0,0 +1,52 @@
+package com.outsider.masterofprediction.service;
+
+import com.outsider.masterofprediction.dto.TblTierDTO;
+import com.outsider.masterofprediction.dto.TblUserDTO;
+import com.outsider.masterofprediction.mapper.TierMapper;
+import com.outsider.masterofprediction.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TierScheduledTasks {
+    private final TierMapper tiMapper;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public TierScheduledTasks(TierMapper tiMapper, UserMapper userMapper) {
+        this.tiMapper = tiMapper;
+        this.userMapper = userMapper;
+    }
+
+    @Scheduled(fixedRate = 300000) // 300000ms = 5분
+    public void updateUserTier() {
+        List<TblTierDTO> tiers = tiMapper.findAll();
+        List<TblUserDTO> users = userMapper.findAllReturnTblUserDTO();
+
+        int totalUsers = users.size();
+
+        for (int i = 0; i < totalUsers; i++) {
+            TblUserDTO user = users.get(i);
+            Long tierNo = getTierForUser(i, totalUsers, tiers);
+            if (tierNo == null){
+                continue;
+            }
+            user.setTierNo(tierNo);
+        }
+        userMapper.bulkUpdateUserTiers(users); // 여기서 users는 List<UserDTO> 타입의 변수
+    }
+
+    private Long getTierForUser(int rank, int totalUsers, List<TblTierDTO> tiers) {
+        int percentage = (int) Math.round((double)(rank + 1) / totalUsers * 100); // 현재 순위의 백분율 계산
+        for (TblTierDTO tier : tiers) {
+            if (percentage >= tier.getTierBegin() && percentage <= tier.getTierEnd()) {
+                return tier.getTierNo();
+            }
+        }
+        return null; // 티어가 없는 경우
+    }
+
+}
