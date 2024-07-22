5466c17bb08f4636d1816e7f052c042384559f8c
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-22 16:14:54 +0900

    feat: 각 작업사항 동기화 (#127)
    * feat: 메인페이지 데이터베이스 연동
    
    * feat: 누락된 DTO 추가
    
    * feat: logo 여백 제거
    
    * feat: 관리자 사이드바에서 항목 클릭 시 색상 변경
    
    * feat: 파일 업로드 폴더 수정
    
    * feat: 헤더의 카테고리 수정
    
    * feat: 헤더 텍스트 수정
    
    * feat: 메인페이지에서 상품 선택시 상세페이지로 이동 url 수정
    
    * feat: 각 페이지 연결

--- src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
@@ -1,6 +1,7 @@
 package com.outsider.masterofprediction.controller;
 
 import com.outsider.masterofprediction.dto.response.MainPageSubjectDTO;
+import com.outsider.masterofprediction.service.CategoryService;
 import com.outsider.masterofprediction.service.MainPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,10 +16,12 @@ import java.util.List;
 public class MainPageController {
 
     private final MainPageService mainPageService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public MainPageController(MainPageService mainPageService) {
+    public MainPageController(MainPageService mainPageService, CategoryService categoryService) {
         this.mainPageService = mainPageService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping()
@@ -30,6 +33,7 @@ public class MainPageController {
 
         mv.setViewName("/layout/main-page/index");
         mv.addObject("subjects", mainPageSubjectDTOS);
+        mv.addObject("categories", categoryService.findAll());
         mv.addObject("title", "Master Of Prediction");
         mv.addObject("view", "content/main-page/main-page");
 
