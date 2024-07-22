966242ce229d994b4a197bf790e18d1a39c85e0f
TAEKWON <57518724+otw7917@users.noreply.github.com>
2024-07-22 16:10:59 +0900

    feat: 배팅 게임 등록 진입점 + 컨트롤러 (#122)
    * feat: 배팅 게임 등록 진입점 + 컨트롤러
    
    * hotfix : 등록 후 페이지 리다이렉션 url 수정

--- src/main/java/com/outsider/masterofprediction/controller/BettingAddPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/BettingAddPageController.java
@@ -1,63 +1,52 @@
 package com.outsider.masterofprediction.controller;
 
-import com.outsider.masterofprediction.dto.BettingAddDTO;
 import com.outsider.masterofprediction.dto.TblCategoryDTO;
-import com.outsider.masterofprediction.service.BettingAddService;
+import com.outsider.masterofprediction.dto.TblSubjectDTO;
+import com.outsider.masterofprediction.service.BettingCreateService;
 import com.outsider.masterofprediction.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Controller
 @RequestMapping("/bettingaddpage")
 public class BettingAddPageController {
 
-    private final BettingAddService bettingAddService;
+    private final BettingCreateService bettingCreateService;
     private final CategoryService categoryService;
 
-    public BettingAddPageController(BettingAddService bettingAddService, CategoryService categoryService) {
-        this.bettingAddService = bettingAddService;
+    public BettingAddPageController(BettingCreateService bettingCreateService, CategoryService categoryService) {
+        this.bettingCreateService = bettingCreateService;
         this.categoryService = categoryService;
     }
 
     @GetMapping()
     public ModelAndView getBettingAddPage(ModelAndView mv) {
-        System.out.println("TEST");
         List<TblCategoryDTO> categoryList = categoryService.findAll();
-        mv.setViewName("/layout/betting-page/add");
+        mv.setViewName("layout/main-page/index");
         mv.addObject("title", "Add Betting Page");
+        mv.addObject("view", "fragments/betting-add");
         mv.addObject("categoryList", categoryList);
         return mv;
     }
 
 
-//    @PostMapping
-//    public String createBetting(@ModelAttribute TblSubjectDTO tblSubjectDTO,
-//                                @RequestParam("attachmentFileAddress") MultipartFile     file,
-//                                @RequestParam("deadline") Date date){
-//        tblSubjectDTO.setSubjectSettlementTimestamp(new Timestamp(date.getTime()));
-//        bettingCreateService.create(tblSubjectDTO, file);
-//        return "redirect:/admin-page/betting";
-//    }
-
     @PostMapping("/create")
-    public String createBettingGame(@ModelAttribute BettingAddDTO bettingAddDTO
-                                    ,@RequestParam("attachmentFileAddress")MultipartFile file
-//                                    ,@RequestParam("date")Date data,
-//                                    ,@RequestParam("time")Time
-    )
-    {
-
-        System.out.println(bettingAddDTO);
-        System.out.println(file);
-//        bettingAddDTO.setSubjectSettlementTimestamp(new TimeStamp);
-//        System.out.println(file);
-//        bettingAddService.create(bettingAddDTO);
-        return "redirect:/mainpage";
+    public String createBetting(@ModelAttribute TblSubjectDTO tblSubjectDTO,
+                                @RequestParam("attachmentFileAddress") MultipartFile file,
+                                @RequestParam("deadlineDate") Date date,
+                                @RequestParam("deadlineTime") String time){
+        tblSubjectDTO.setSubjectSettlementTimestamp(Timestamp.valueOf(LocalDateTime.of(LocalDate.parse(date.toString()), LocalTime.parse(time))));;
+        bettingCreateService.create(tblSubjectDTO, file);
+        return "redirect:/";
     }
-
 
 }
