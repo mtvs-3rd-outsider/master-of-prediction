fb642e7befbf81f42ace4cd7aad0e7ac70ac95ae
윤여원 <dudnjsckrgo@gmail.com>
2024-07-23 20:31:29 +0900

    feat: 헤더 포인트 연동 #171

--- src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/MainPageController.java
@@ -1,12 +1,14 @@
 package com.outsider.masterofprediction.controller;
 
 import com.outsider.masterofprediction.dto.ActivityUserSubjectDTO;
+import com.outsider.masterofprediction.dto.CustomUserDetail;
 import com.outsider.masterofprediction.dto.constatnt.IntConstants;
 import com.outsider.masterofprediction.dto.response.MainPageSubjectDTO;
 import com.outsider.masterofprediction.mapper.BettingOrderMapper;
 import com.outsider.masterofprediction.service.CategoryService;
 import com.outsider.masterofprediction.service.MainPageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +32,7 @@ public class MainPageController {
     }
 
     @GetMapping()
-    public ModelAndView getMainPage(ModelAndView mv){
+    public ModelAndView getMainPage(ModelAndView mv,@AuthenticationPrincipal CustomUserDetail user){
 
         // mv.addObject();
         List<MainPageSubjectDTO> mainPageSubjectDTOS =  mainPageService.findMainPageSubjectList();
@@ -44,7 +46,7 @@ public class MainPageController {
         mv.addObject("title", "Master Of Prediction");
         mv.addObject("activity", activityUserSubjectDTO);
         mv.addObject("view", "content/main-page/main-page");
-
+        mv.addObject("Point",user.getPoint());
         return mv;
     }
 }
