49a21c6599f860bd03f2a0da3a7a64c69d4adf0e
apfp77 <apfp77@naver.com>
2024-07-23 20:39:52 +0900

    feat: 배팅 결과발표 ui 추가 및 배팅 수정 일부 텍스트 수정

--- src/main/java/com/outsider/masterofprediction/controller/AdminBettingResultController.java
+++ src/main/java/com/outsider/masterofprediction/controller/AdminBettingResultController.java
@@ -6,16 +6,23 @@ import com.outsider.masterofprediction.service.BettingService;
 import com.outsider.masterofprediction.service.BettingUpdateService;
 import com.outsider.masterofprediction.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.nio.file.Paths;
 import java.sql.Date;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Controller
 public class AdminBettingResultController {
-
+    @Value("${file.imgUrl}")
+    private String imgUrl;
     private final BettingUpdateService bettingUpdateService;
     private final BettingService bettingService;
     private final CategoryService categoryService;
@@ -27,29 +34,29 @@ public class AdminBettingResultController {
         this.categoryService = categoryService;
     }
 
-    // @PostMapping("/admin-page/betting/result/{subjectNo}")
-    // public String bettingResult(@PathVariable String result, @RequestParam Long subjectNo) {
-    //     // 결과 저장 로직
-    //     System.out.println(result);
-    //     System.out.println(subjectNo);
-    //     // subject에 결과값과 시간, 설정
-    //
-    //     // bettingUpdateService.finish(result, subjectNo);
-    //
-    //     // 포인트 계산 로직
-    //     // 소모된 포인트를 기준으로 비율계산하여 반환
-    //
-    //     return "redirect:/admin-page/betting";
-    // }
+    @PostMapping("/admin-page/betting/result/{subjectNo}")
+    public String bettingResult(@PathVariable Long subjectNo, @RequestParam String result) {
+        System.out.println(subjectNo);
+        System.out.println(result);
+
+
+        return "redirect:/admin-page/betting";
+    }
 
 
     @GetMapping("/admin-page/betting/result/{subjectNo}")
     public String bettingResult(@PathVariable Long subjectNo, Model model) {
-        BettingAndAttachmentDTO bettingDetail = bettingService.getBettingAndAttachmentBySubjectNo(subjectNo);
-        TblCategoryDTO categoryDTO = categoryService.findByCategoryNo(bettingDetail.getSubjectCategoryNo());
-        model.addAttribute("betting", bettingDetail);
-        model.addAttribute( "category", categoryDTO);
-        model.addAttribute("deadline", new Date(bettingDetail.getSubjectSettlementTimestamp().getTime()));
+        BettingAndAttachmentDTO betting = bettingService.getBettingAndAttachmentBySubjectNo(subjectNo);
+
+        betting.setAttachmentFileAddress(Paths.get(imgUrl).resolve(betting.getAttachmentFileAddress()).toString());
+
+        ZonedDateTime zonedDateTime = betting.getSubjectSettlementTimestamp().toInstant().atZone(ZoneId.systemDefault());
+
+
+        model.addAttribute("betting", betting);
+        model.addAttribute("deadlineDate", zonedDateTime.toLocalDate());
+        model.addAttribute("deadlineTime", zonedDateTime.toLocalTime());
+        model.addAttribute("categories", categoryService.findAll());
 
         return "content/admin-page/betting/result";
     }
