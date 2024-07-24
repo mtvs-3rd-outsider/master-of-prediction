3a0f37891cb85716837371d3b55bff57f063ad13
daekwon2000 <daekklove@gmail.com>
2024-07-24 14:47:56 +0900

    159-searching-word-based-search

---
+++ src/main/java/com/outsider/masterofprediction/mapper/SearchPageMapper.java
@@ -0,0 +1,15 @@
+package com.outsider.masterofprediction.mapper;
+
+import com.outsider.masterofprediction.dto.CategoryPageSubjectDTO;
+import com.outsider.masterofprediction.dto.SearchPageSubjectDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface SearchPageMapper {
+
+    List<SearchPageSubjectDTO> findSearchPageSubjectList(String searchWord);
+
+}
