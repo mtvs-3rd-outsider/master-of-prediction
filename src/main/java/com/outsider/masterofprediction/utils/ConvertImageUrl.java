cef83a0e583148ba468a26ef5248e8efde9e4d50
otw7917 <otw7917@gmail.com>
2024-07-24 16:05:51 +0900

    feat: 상품이 존재하나 이미지가 존재하지 않을 경우 추가

--- src/main/java/com/outsider/masterofprediction/utils/ConvertImageUrl.java
+++ src/main/java/com/outsider/masterofprediction/utils/ConvertImageUrl.java
@@ -1,5 +1,7 @@
 package com.outsider.masterofprediction.utils;
 
+import com.outsider.masterofprediction.dto.TblAttachmentDTO;
+import com.outsider.masterofprediction.dto.UserAttachmentDTO;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,6 +11,9 @@ public class ConvertImageUrl {
 
     public static <T extends AttachmentFileAddressable> void convert(List<T> items){
         for (T item : items){
+            if (item instanceof UserAttachmentDTO){
+                ((UserAttachmentDTO) item).setAttachment(new TblAttachmentDTO());
+            }
             if (item.getAttachmentFileAddress() == null){
                 item.setAttachmentFileAddress("logo2.png");
             }
