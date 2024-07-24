98e24f92ab5d8fa1515e05af760a613825347db6
apfp77 <apfp77@naver.com>
2024-07-24 15:11:59 +0900

    feat: 동기화

--- src/main/java/com/outsider/masterofprediction/utils/ConvertImageUrl.java
+++ src/main/java/com/outsider/masterofprediction/utils/ConvertImageUrl.java
@@ -9,8 +9,13 @@ public class ConvertImageUrl {
 
     public static <T extends AttachmentFileAddressable> void convert(List<T> items){
         for (T item : items){
+            if (item.getAttachmentFileAddress() == null){
+                item.setAttachmentFileAddress("logo2.png");
+                System.out.println(item);
+            }
             item.setAttachmentFileAddress(
                     FileUtil.checkFileOrigin(item.getAttachmentFileAddress()));
+            System.out.println(item);
         }
     }
 
