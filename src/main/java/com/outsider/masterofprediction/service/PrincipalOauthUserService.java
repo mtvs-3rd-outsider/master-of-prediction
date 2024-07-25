15587228f054f82ca619ea39300fff9855cfdaed
윤여원 <dudnjsckrgo@gmail.com>
2024-07-25 22:27:34 +0900

    fix: Oauth 로그인 시 사용자가 null로 나오는 현상 해결

--- src/main/java/com/outsider/masterofprediction/service/PrincipalOauthUserService.java
+++ src/main/java/com/outsider/masterofprediction/service/PrincipalOauthUserService.java
@@ -34,10 +34,7 @@ public class PrincipalOauthUserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         //"registraionId" 로 어떤 OAuth 로 로그인 했는지 확인 가능(google,naver등)
-        System.out.println("getClientRegistration: "+userRequest.getClientRegistration());
-        System.out.println("getAccessToken: "+userRequest.getAccessToken().getTokenValue());
-        System.out.println("getAttributes: "+ super.loadUser(userRequest).getAttributes());
-        //구글 로그인 버튼 클릭 -> 구글 로그인창 -> 로그인 완료 -> code를 리턴(OAuth-Clien라이브러리가 받아줌) -> code를 통해서 AcssToken요청(access토큰 받음)
+         //구글 로그인 버튼 클릭 -> 구글 로그인창 -> 로그인 완료 -> code를 리턴(OAuth-Clien라이브러리가 받아줌) -> code를 통해서 AcssToken요청(access토큰 받음)
         // => "userRequest"가 감고 있는 정보
         //회원 프로필을 받아야하는데 여기서 사용되는것이 "loadUser" 함수이다 -> 구글 로 부터 회원 프로필을 받을수 있다.
 
@@ -79,6 +76,9 @@ public class PrincipalOauthUserService extends DefaultOAuth2UserService {
 
             userMapper.createUser(user.getName(), user.getEmail(),  user.getPassword(), user.getAuthority());
             User user1=userMapper.findByEmail(email);
+            if (user1 != null){
+                user.setId(user1.getId());
+            }
             if(!pictureUrl.equals(""))
             {
                 TblAttachmentDTO tblAttachmentDTO = new TblAttachmentDTO();
