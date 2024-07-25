d50a9922538e8ac6f43446525873e79620d7657c
jeonjinhyun <jjh06500@gmail.com>
2024-07-25 14:58:17 +0900

    feat : nullException (#234)

--- src/main/java/com/outsider/masterofprediction/service/CommentService.java
+++ src/main/java/com/outsider/masterofprediction/service/CommentService.java
@@ -1,9 +1,6 @@
 package com.outsider.masterofprediction.service;
 
-import com.outsider.masterofprediction.dto.CommentDTO;
-import com.outsider.masterofprediction.dto.CommentReDTO;
-import com.outsider.masterofprediction.dto.TblCommentDTO;
-import com.outsider.masterofprediction.dto.TblReplyDTO;
+import com.outsider.masterofprediction.dto.*;
 import com.outsider.masterofprediction.mapper.CommentMapper;
 import com.outsider.masterofprediction.utils.FileUtil;
 import jdk.dynalink.beans.StaticClass;
@@ -23,16 +20,31 @@ public class CommentService {
         this.userManagementService = userManagementService;
     }
 
-    public List<CommentReDTO> getCommentBySubjectNo(long subjectNo){
-        List<CommentReDTO> list =commentMapper.getCommentBySubjectNo(subjectNo);
-        for(CommentReDTO dto: list){
-            dto.setImgUrl(FileUtil.checkFileOrigin(userManagementService.getAttachmentsByUserNo(dto.getUserNo()).getAttachmentFileAddress()));
-            if(dto.getReplyUserNo()!=0){
-                dto.setReplyImgUrl(FileUtil.checkFileOrigin(userManagementService.getAttachmentsByUserNo(dto.getReplyUserNo()).getAttachmentFileAddress()));
+    public List<CommentReDTO> getCommentBySubjectNo(long subjectNo) {
+        List<CommentReDTO> list = commentMapper.getCommentBySubjectNo(subjectNo);
+
+        for (CommentReDTO dto : list) {
+            TblAttachmentDTO attachment = userManagementService.getAttachmentsByUserNo(dto.getUserNo());
+            if (attachment != null && attachment.getAttachmentFileAddress() != null) {
+                dto.setImgUrl(FileUtil.checkFileOrigin(attachment.getAttachmentFileAddress()));
+            } else {
+                // 필요에 따라 기본 이미지 URL을 설정하거나 아무것도 하지 않음
+                dto.setImgUrl("/images/upload/logo2.png"); // 기본 이미지 URL 설정
+            }
+
+            if (dto.getReplyUserNo() != 0) {
+                TblAttachmentDTO replyAttachment = userManagementService.getAttachmentsByUserNo(dto.getReplyUserNo());
+                if (replyAttachment != null && replyAttachment.getAttachmentFileAddress() != null) {
+                    dto.setReplyImgUrl(FileUtil.checkFileOrigin(replyAttachment.getAttachmentFileAddress()));
+                } else {
+                    // 필요에 따라 기본 이미지 URL을 설정하거나 아무것도 하지 않음
+                    dto.setReplyImgUrl("/images/upload/logo2.png"); // 기본 이미지 URL 설정
+                }
             }
         }
         return list;
     }
+
 
     public void insertComment(TblCommentDTO commentDTO) {
         commentMapper.insertComment(commentDTO);
