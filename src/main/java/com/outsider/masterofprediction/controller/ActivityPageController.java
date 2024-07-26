d90e7aaaaded6986f4112abd8cfa1d83c26ede3c
apfp77 <78768234+apfp77@users.noreply.github.com>
2024-07-26 20:28:40 +0900

    249 feat activity rank css (#250)
    * feat: application.properties 경로 수정
    
    * feat: Rank, ActivityPage css 설정
    
    * feat: gitignore 추가

--- src/main/java/com/outsider/masterofprediction/controller/ActivityPageController.java
+++ src/main/java/com/outsider/masterofprediction/controller/ActivityPageController.java
@@ -1,6 +1,8 @@
 package com.outsider.masterofprediction.controller;
 
 import com.outsider.masterofprediction.dto.ActivityUserSubjectDTO;
+import com.outsider.masterofprediction.dto.constatnt.IntConstants;
+import com.outsider.masterofprediction.mapper.BettingOrderMapper;
 import com.outsider.masterofprediction.service.ActivityFindService;
 import com.outsider.masterofprediction.service.CategoryService;
 import com.outsider.masterofprediction.utils.ConvertImageUrl;
@@ -18,25 +20,27 @@ public class ActivityPageController {
 
     private final ActivityFindService activityFindService;
     private final CategoryService categoryService;
+    private final BettingOrderMapper bettingOrderMapper;
 
-    public ActivityPageController(ActivityFindService activityFindService, CategoryService categoryService) {
+    public ActivityPageController(ActivityFindService activityFindService, CategoryService categoryService, BettingOrderMapper bettingOrderMapper) {
         this.activityFindService = activityFindService;
         this.categoryService = categoryService;
+        this.bettingOrderMapper = bettingOrderMapper;
     }
 
     @GetMapping
     public ModelAndView getActivityPage(ModelAndView mv) {
+        List<ActivityUserSubjectDTO> activityUserSubjectDTO =
+                bettingOrderMapper.findActivityCount(IntConstants.MAIN_PAGE_ACTIVITY_COUNT);
 
-        mv.setViewName("/layout/index");
-        mv.addObject("title", "Recent Activity");
-        mv.addObject("view", "content/activity-page");
-        List<ActivityUserSubjectDTO> userActivity = activityFindService.findActivity();
-        for (ActivityUserSubjectDTO tblAttachmentDTO: userActivity) {
+        for (ActivityUserSubjectDTO tblAttachmentDTO: activityUserSubjectDTO) {
             tblAttachmentDTO.getAttachmentUser().setAttachmentFileAddress(FileUtil.checkFileOrigin(tblAttachmentDTO.getAttachmentUser().getAttachmentFileAddress()));
             tblAttachmentDTO.getAttachmentSubject().setAttachmentFileAddress(FileUtil.checkFileOrigin(tblAttachmentDTO.getAttachmentSubject().getAttachmentFileAddress()));
         }
-        mv.addObject("userActivity", userActivity);
-
+        mv.setViewName("/layout/index");
+        mv.addObject("title", "Recent Activity");
+        mv.addObject("activity", activityUserSubjectDTO);
+        mv.addObject("view", "fragments/activity-content");
 
         return mv;
     }
