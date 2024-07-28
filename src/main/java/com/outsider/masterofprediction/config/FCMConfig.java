7fafd04d9c920b4e84a3dc5aca9dd4e6368747f3
윤여원 <dudnjsckrgo@gmail.com>
2024-07-28 19:44:39 +0900

    style:firebase confgi path 프로퍼티 변수로

--- src/main/java/com/outsider/masterofprediction/config/FCMConfig.java
+++ src/main/java/com/outsider/masterofprediction/config/FCMConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -16,10 +17,12 @@ import java.util.List;
 @Configuration
 public class FCMConfig {
 
+    @Value("{firebase.config.path}")
+    private String firebaseConfigPath;
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase/master-of-prediction-384d0-firebase-adminsdk-47mce-780f4ac1f3.json");
+                new FileInputStream(firebaseConfigPath);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
